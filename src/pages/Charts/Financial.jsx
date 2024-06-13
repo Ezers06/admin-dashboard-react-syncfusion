c665bcf987dab9dc35cb12852a97fa5f23a80d76
Ameer Rizal <92781236+Ezers06@users.noreply.github.com>
2024-06-13 18:14:01 +0800

    Create An Admin Dashboard Project  using Syncfusion UI

---
+++ src/pages/Charts/Financial.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  HiloSeries,
+  Tooltip,
+  DateTime,
+  Zoom,
+  Logarithmic,
+  Crosshair,
+} from "@syncfusion/ej2-react-charts";
+import {
+  financialChartData,
+  FinancialPrimaryXAxis,
+  FinancialPrimaryYAxis,
+} from "../../shared-assets/dummy";
+import { Header } from "../../components";
+import { useStateContext } from "../../contexts/ContextProvider";
+
+const date1 = new Date("2007, 1, 1");
+
+function filterValue(value) {
+  if (value.x >= date1) {
+    return value.x, value.high, value.low;
+  }
+}
+const returnValue = financialChartData.filter(filterValue);
+
+const Financial = () => {
+  const { currentMode } = useStateContext();
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category={"Chart"} title={"Financial Chart"} />
+      <div className="w-full">
+        <ChartComponent
+          id="Charts"
+          primaryXAxis={FinancialPrimaryXAxis}
+          primaryYAxis={FinancialPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true, shared: true }}
+          crosshair={{ enable: true, lineType: "Vertical", line: { width: 0 } }}
+          background={currentMode === "Dark" ? "#33373E" : "#FFF"}
+        >
+          <Inject
+            services={[
+              HiloSeries,
+              Tooltip,
+              DateTime,
+              Logarithmic,
+              Crosshair,
+              Zoom,
+            ]}
+          />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={returnValue}
+              xName="x"
+              yName="y"
+              name="Apple Inc"
+              type="Hilo"
+              low="low"
+              high="high"
+            />
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
+  );
+};
+
+export default Financial;
